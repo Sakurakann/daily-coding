0015d83fdf949279a935583eb47f608bae3ff5ef
haiyangp <haiyangp24@163.com>
2018-02-05 15:38:07 +0800

    跟新sql语句

---
+++ base_crud/src/main/java/org/sknn/base/domain/User.java
@@ -0,0 +1,10 @@
+package org.sknn.base.domain;
+
+/**
+ * Created with IntelliJ IDEA. Project: daily-coding. Package:
+ * org.sknn.base.domain. User: Administrator. Date: 2018-02-05 09:56. Author:
+ * Haiyangp.
+ */
+public class User {
+
+}
